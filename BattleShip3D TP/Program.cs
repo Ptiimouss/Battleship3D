0edb32c0ae35e088627059b7473113a73b78a1b6
Gaétan Tapia <gaetan.tapia@gmail.com>
2025-03-12 18:43:13 +0100

    Ajout du package MySql.Data

---
+++ BattleShip3D TP/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+namespace BattleShip3D_TP
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
